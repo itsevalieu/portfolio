8b0bd97b25d4ff5f804bb87e5cde5b2b5550bdfa
Eva Lieu <itsevalieu@gmail.com>
2019-04-01 22:29:05 -0700

    Grid template for desktop; edit routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent },
+  { path: 'portfolio', component: PortfolioComponent },
   { path: 'contact', component: ContactComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { NavComponent } from './nav/nav.component';
 import { ContactComponent } from './contact/contact.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
     NavComponent,
     ContactComponent,
-    ProjectsComponent
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
