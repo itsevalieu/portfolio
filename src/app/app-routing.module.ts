72dd6a751d1684155a6bdee588e261f62479ac2f
itsevalieu <itsevalieu@gmail.com>
2019-04-05 20:06:33 -0700

    Add home routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'portfolio', component: PortfolioComponent },
-  { path: 'contact', component: ContactComponent}
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
